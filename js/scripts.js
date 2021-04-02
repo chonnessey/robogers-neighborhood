1380f383785ac47efbf471fa71ee83fcacdc0768
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-02 16:12:09 -0700

    add css styles to buttonand comment out scripts functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 // Business Logic
-const phraseArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-let inputtedArray = [];
-let string = (" ")
+const phraseArray = ["0", "Beep!", "Boop!", "Won't you be my neighbor?"];
+let inputtedString = ("");
+
 
 function beepBoop(text) {
   if (text === 0); 
-    string = ("0");
+    inputtedString = "0";
 }
-function beepBoop(text) {
-  if (text === 1);
-  return "Beep!";
-}
-function beepBoop(text) {
-  if (text === 2);
-  return "Boop!";
-}
-function beepBoop(text) {
-  if (text === 3);
-  return "Won't you be my neighbor?";
-}
+// function beepBoop(text) {
+//   if (text === 1);
+//   return "Beep!";
+// }
+// function beepBoop(text) {
+//   if (text === 2);
+//   return "Boop!";
+// }
+// function beepBoop(text) {
+//   if (text === 3);
+//   return "Won't you be my neighbor?";
+// }
 
 // UI logic
 $(document).ready(function() {
