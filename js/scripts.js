2629cdf54bb1db9dedcd948bcefa72f418509009
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-02 14:54:05 -0700

    add code that passed test 2

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,10 @@ const phraseArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
 let inputtedArray = [];
 
 function beepBoop(text) {
-  if (text === 0); {
-    return inputtedArray [0];
-  }
+  if (text === 0); 
+    return "0";
 }
-
-function beepBop(text) {
-  if (text === 1); {
-    
-  }
+function beepBoop(text) {
+  if (text === 1);
+  return "Beep!";
 }
