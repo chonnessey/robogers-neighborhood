6d22117ea1bafae5b47c5625bb36eeaa0ebda7f8
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-02 13:56:07 -0700

    WIP: I am completely stumped...

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 // Business Logic
 const phraseArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+let inputtedArray = [];
 
 function beepBoop(text) {
   if (text === 0);
-    return [0];
+    return inputtedArray = ["0"];
 }
-let inputtedArray = [];
-function beepBoop(inputtedArray, text) {
-  phraseArray.forEach(function(text) {
-    if (text.includes(1)) {
-      inputtedArray = ["Beep!"];
+phraseArray.forEach(function(text) {
+    if (text === 1) {
+      return inputtedArray = ["Beep!"];
     }
-  });
-}
+  
+});
