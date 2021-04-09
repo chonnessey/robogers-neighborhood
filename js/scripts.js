ac425c161dddb2bec1c6f749c5b4f06b2a6c21ff
Adrian Camacho <adriancamacho18@gmail.com>
2021-04-09 15:11:23 -0700

    WIP: still trying to figure the functionality out

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 // Business Logic
-const phraseArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function beepBoop(text) {
+  // const phraseArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+  const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const number = text.toString(" ");
+  for (let i = 0; i < numbersArray.length; i+=1) {
+    if (numbersArray[i] === number[0]) {
+      number.toString("Beep!")
+    }
+  }
+
+}
+
+
 // numberArray.splice(0, 1, "Beep!")
 // console.log(numberArray);
-let inputtedArray = [];
+// let inputtedArray = [];
 
 
-function beepBoop(text) {
+// function beepBoop(number) 
+
 //   if (text === 0); 
 //     return inputtedArray = [0];
 // }
